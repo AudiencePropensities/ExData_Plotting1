3bb3f88f6fd38c6e6fe617aa383d4fbd0a091b9a
Wu Fred-fredwu <fred.wu@acxiom.com>
2014-09-07 17:03:39 -0500

    remove unused directory

--- other/plot3.r
+++
@@ -1,4 +0,0 @@
-setwd("C:/Users/Frederick/Documents/RStudio/data/coursera/expoloratory_data_analysis")
-data1<-read.table("household_power_consumption.txt",header=T,sep=";")
-data2<-subset(data1,Date %in% c("2/2/2007","1/2/2007"))
-hist(as.numeric(data2$Global_active_power),breaks=12,xlab="Global_Active_Power(Kilowatts)",col="red")
