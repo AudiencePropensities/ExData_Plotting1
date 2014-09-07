22a30efa6b4421b3ae0ee93cd6070e97d551f0a1
Wu Fred-fredwu <fred.wu@acxiom.com>
2014-09-07 16:58:10 -0500

    adding R code

--- plot2.r
+++ plot2.r
@@ -1,4 +1,19 @@
-setwd("C:/Users/Frederick/Documents/RStudio/data/coursera/expoloratory_data_analysis")
-data1<-read.table("household_power_consumption.txt",header=T,sep=";")
-data2<-subset(data1,Date %in% c("2/2/2007","1/2/2007"))
-hist(as.numeric(data2$Global_active_power),breaks=12,xlab="Global_Active_Power(Kilowatts)",col="red")
+## Read full dataset
+setwd("/Users/fredwu/Documents/COURSE")
+data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                 stringsAsFactors=F, quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+head(data)
+
+## Subset the data
+data1 <- subset(data, subset=(Date == "2007-02-01" | Date == "2007-02-02"))
+rm(data)
+
+## Convert dates
+data1$Datetime <- as.POSIXct(paste(as.Date(data1$Date), data1$Time))
+
+## Plot 2
+attach(data1)
+png("plot2.png", height=480, width=480)
+plot(Global_active_power~Datetime, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
--- plot3.r
+++ plot3.r
@@ -1,4 +1,24 @@
-setwd("C:/Users/Frederick/Documents/RStudio/data/coursera/expoloratory_data_analysis")
-data1<-read.table("household_power_consumption.txt",header=T,sep=";")
-data2<-subset(data1,Date %in% c("2/2/2007","1/2/2007"))
-hist(as.numeric(data2$Global_active_power),breaks=12,xlab="Global_Active_Power(Kilowatts)",col="red")
+## Read full dataset
+setwd("/Users/fredwu/Documents/COURSE")
+data <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                 stringsAsFactors=F, quote='\"')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+head(data)
+
+## Subset the data
+data1 <- subset(data, subset=(Date == "2007-02-01" | Date == "2007-02-02"))
+rm(data)
+
+## Convert dates
+data1$Datetime <- as.POSIXct(paste(as.Date(data1$Date), data1$Time))
+
+## Plot 3
+attach(data1)
+png("plot3.png", height=480, width=480)
+plot(Sub_metering_1~Datetime, type="l", xlab="", ylab="Energy sub metering")
+lines(Sub_metering_2~Datetime,col='Red')
+lines(Sub_metering_3~Datetime,col='Blue')
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),cex=1)
+dev.off()
+
